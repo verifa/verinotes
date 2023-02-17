99fedcbbf1fa028ecc71fdd4fab1c83038fce92f
Mike Vainio <mvainio@verifa.io>
2023-02-17 13:37:00 +0200

    working httpStore for fetching notes and ui routed to backend in dev

---
+++ ui/src/lib/http/store.ts
@@ -0,0 +1,112 @@
+import { writable } from 'svelte/store'
+
+export interface Response<Data> {
+    fetching: boolean
+    ok: boolean
+    // requested is marked as true after at least one request has been made
+    requested: boolean
+    data?: Data
+    status?: number
+    text?: string
+    error?: Error
+    redirected?: boolean
+}
+
+export function createHttpStore<Data>() {
+    const store = writable<Response<Data>>({
+        fetching: false,
+        ok: false,
+        requested: false,
+    })
+
+    function request(method: string, path: string, params?: Record<string, string>, data?: object) {
+        // Clear the store as we are about to make a new request
+        store.update((value) => {
+            value.ok = false
+            value.fetching = true;
+            value.requested = true;
+            value.error = undefined;
+            value.data = undefined;
+            return value;
+        });
+
+        // vite proxy is used in dev to route traffic to the backend, see vite.config.js
+        let url = "/api/v1" + path
+
+        const headers = {
+            "Content-type": "application/json"
+        }
+        // If we received query params, add them to the url
+        if (params) {
+            url = url + "?" + new URLSearchParams(params)
+        }
+        const body = data ? JSON.stringify(data) : undefined
+
+        fetch(url, {
+            method, body, headers,
+            // TODO: this could be same-origin when running on the same site
+            credentials: 'include'
+        }).then((response) => {
+            if (response.ok) {
+                if (response.redirected) {
+                    store.update((value) => {
+                        value.ok = response.ok
+                        value.status = response.status
+                        value.redirected = response.redirected
+                        return value
+                    })
+                    return
+                }
+                response.json().then((data) => {
+                    store.update((value) => {
+                        value.fetching = false
+                        value.status = response.status
+                        value.ok = response.ok
+                        value.data = data
+                        console.log(value)
+                        return value
+                    })
+                }).catch((error) => {
+                    store.update((value) => {
+                        value.fetching = false
+                        value.error = error
+                        value.text = "Error converting response to JSON"
+                        return value
+                    })
+                })
+            } else {
+                response.text().then((text) => {
+                    store.update((value) => {
+                        value.fetching = false
+                        value.status = response.status
+                        value.ok = response.ok
+                        value.text = text
+                        return value
+                    })
+                }).catch((error) => {
+                    store.update((value) => {
+                        value.fetching = false
+                        value.error = error
+                        value.text = "Error converting response to text"
+                        return value
+                    })
+                })
+            }
+        })
+            .catch((error) => {
+                store.update((value) => {
+                    value.fetching = false
+                    value.error = error
+                    return value
+                })
+            });
+    }
+
+    return {
+        ...store,
+        get: (path: string, params?: Record<string, string>, data?: object) =>
+            request('GET', path, params, data),
+        post: (path: string, params?: Record<string, string>, data?: object) =>
+            request('POST', path, params, data),
+    };
+}
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,39 +1,46 @@
-<h1>Welcome to VeriNotes!</h1>
-<p>Visit <a href="https://verifa.io">verifa.io</a> to learn more about Verifa</p>
+<script lang="ts">
+	import { createHttpStore } from '$lib/http/store';
+	import { onMount } from 'svelte';
 
-<div class="card w-96 bg-base-100 shadow-xl my-4">
-    <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
-        <p>Super important note</p>
-        <div class="card-actions justify-end">
-        <button class="btn btn-primary">Click!</button>
-        </div>
-    </div>
-</div>
-<div class="card w-96 bg-base-100 shadow-xl my-4">
-    <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
-        <p>Super important note</p>
-        <div class="card-actions justify-end">
-        <button class="btn btn-primary">Click!</button>
-        </div>
-    </div>
-</div>
-<div class="card w-96 bg-base-100 shadow-xl my-4">
-    <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
-        <p>Super important note</p>
-        <div class="card-actions justify-end">
-        <button class="btn btn-primary">Click!</button>
-        </div>
-    </div>
-</div>
-<div class="card w-96 bg-base-100 shadow-xl my-4">
-    <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
-        <p>Super important note</p>
-        <div class="card-actions justify-end">
-        <button class="btn btn-primary">Click!</button>
-        </div>
-    </div>
-</div>
+	type NotesResp = [{
+		id: string;
+		data: string;
+	}]
+
+	const notesStore = createHttpStore<NotesResp>();
+	onMount(async () => {
+		console.log("get notes!")
+		notesStore.get('/notes');
+	})
+
+</script>
+
+<h1>Welcome to VeriNotes!</h1>
+<!-- <p>Visit <a href="https://verifa.io">verifa.io</a> to learn more about Verifa</p> -->
+
+{#if $notesStore.fetching}
+	<h2>Loading projects</h2>
+{:else if $notesStore.ok && $notesStore.data}
+	<div class="overflow-x-auto w-full">
+		<table class="table table-auto w-full">
+			<tbody>
+				{#each $notesStore.data as note}
+					<tr>
+						<div class="card w-96 bg-base-100 shadow-xl my-4 border border-gray-200 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+							<div class="card-body">
+								<h2 class="card-title">Note!</h2>
+								<p>{note.data}</p>
+								<div class="card-actions justify-end">
+								<button class="btn btn-primary">Click!</button>
+								</div>
+							</div>
+						</div>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
+{/if}
+
+
+<!-- TODO: button to add notes <a href="{base}/projects/new" class="btn btn-primary">New Project</a> -->
